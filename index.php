15eeb0be5e8fa1c3e52112b0f7cf8aadc6e84c6a
cesarvelame <cesarvelame@gmail.com>
2014-10-02 15:07:13 -0300

    Início

---
+++ index.php
@@ -0,0 +1,28 @@
+                                           <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-PT" lang="pt-PT">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>César Velame - Joomla! / Wordpress / Moodle / Mobile</title>
+<meta name="Keywords" content="césar, velame, carvalho, joomla, software livre, wordpress, moodle, lms, ead" />
+<meta name="Description" content="César Velame de Carvalho" />
+<link href="css/css.css" rel="stylesheet" type="text/css" />
+<!-- GOOGLE ANALYTICS-->
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1905374-5";
+urchinTracker();
+</script>
+<!-- FIM - GOOGLE ANALYTICS-->
+<link rel="shortcut icon" href="http://www.cesarvelame.com/images/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<div id="home">
+<div class="cesar">CÉSAR VELAME</div>
+<div class="profissao">JOOMLA! / WORDPRESS / MOODLE / MOBILE</div>
+<div class="msn">EMAIL:<span class="profissao"> CESARVELAME@GMAIL.COM</span></div>
+<div class="twitter"><a href="http://www.twitter.com/cesarvelame" title="Twitter" target="Default">TWITTER</a></div>
+<div class="facebook"><a href="http://www.facebook.com/cesarvelame" title="Facebook" target="Default">FACEBOOK</a></div>
+</div>
+</body>
+</html>
